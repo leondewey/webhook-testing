5f4131ad6d63c22cdeb49f596914dfda823db2b1
a7f0ca <8cf2dc@example.com>
2016-04-29 11:44:56 -0700

    Commit message: 59f01afd5c7188deb73d5559ec25258e

--- make-events.rb
+++ make-events.rb
@@ -2,7 +2,6 @@ require 'securerandom'
 
 5.times do
   author = "#{SecureRandom.hex[0..5]} <#{SecureRandom.hex[0..5]}@example.com>"
-  puts author
   `touch ./random/#{SecureRandom.hex}`
   `git add -A; git commit -m "Commit message: #{SecureRandom.hex}" --author="#{author}"; git push`
 end
