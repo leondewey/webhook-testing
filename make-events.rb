bae3c79cfe92bdf06aeae4689ebebdfcc9b29ac8
Name <name@example.com>
2016-04-29 11:44:34 -0700

    Commit message: 853b5337f8d92692918f1a9e04e09500

--- make-events.rb
+++ make-events.rb
@@ -1,6 +1,9 @@
 require 'securerandom'
 
-100.times do
+5.times do
+  # author = "#{SecureRandom.hex[0..5]} #{SecureRandom.hex[0..5]}@example.com"
+  author = "Name <name@example.com>"
+  puts author
   `touch ./random/#{SecureRandom.hex}`
-  `git add -A; git commit -m "Commit message: #{SecureRandom.hex}"; git push`
+  `git add -A; git commit -m "Commit message: #{SecureRandom.hex}" --author="#{author}"; git push`
 end
