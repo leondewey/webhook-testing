2e70bcbc9e39efdd288b41af0c420dc00cfca97b
Mrs. Agustin Koepp <kobe_ankunding@mante.net>
2016-06-02 13:37:00 -0700

    Ameliorated mission-critical knowledge user

--- make-events.rb
+++ make-events.rb
@@ -3,8 +3,8 @@ require 'faker'
 
 1.times do
   author = "#{Faker::Name.name} <#{Faker::Internet.email}>"
-  msg = Faker::Lorem.sentence
-  
+  msg = Faker::Company.catch_phrase
+
   `touch ./random/#{SecureRandom.hex}`
   `git add -A; git commit -m "#{msg}" --author="#{author}"; git push`
 end
